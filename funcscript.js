c4c76e9007fea5a18e827abe9329c61ec4cd0b14
luke <71384528+luke333z@users.noreply.github.com>
2024-03-11 00:57:53 +0200

    Keybind Handler

--- funcscript.js
+++ funcscript.js
@@ -166,7 +166,7 @@ const wmDesc = `In "Word Mode," players decode entire words, emphasizing complet
 
 const lmDesc = `In "Letter Mode," players decode words letter by letter, being beginner friendly. `;
 
-const validKeys = ['0','1','2','3','4','5','6','7','8','9','-','=','/','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+const validKeys = ['0','1','2','3','4','5','6','7','8','9','-','=','/',',','.','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 let output = "";
 let COUNTER = 0;
 let blockInput = false;
@@ -415,48 +415,123 @@ function startUp()
 
 //key handler
 let key;
-function waitKey() {
-    return new Promise((resolve) => {
-      document.addEventListener('keydown', onKeyHandler);
-      function onKeyHandler(e) {
-        if (e.key && key != dashKey && key != spaceKey && validKeys.includes(key)) {
-          document.removeEventListener('keydown', onKeyHandler);
-          key = e.key;
-          resolve();
-          
-        }
-      }
-    });
-  }
 
+function checkKeys(){
+    if(key == dotKey)
+        document.getElementById("dotKey").src="./icons/invalidKey.png", dotKey = ""; 
+    if(key == dashKey)
+        document.getElementById("dashKey").src="./icons/invalidKey.png", dashKey = ""; 
+    if(key == spaceKey)
+        document.getElementById("spaceKey").src="./icons/invalidKey.png", spaceKey = ""; 
+}
+function waitKey(con) {
+    return new Promise((resolve) => {
+        document.addEventListener('keydown', onKeyHandler);
+        function onKeyHandler(e) 
+        {
+            if (e.key && validKeys.includes(e.key)) 
+            {
+                document.removeEventListener('keydown', onKeyHandler);
+                key = e.key;
+                checkKeys();
+                resolve();
+            }
+            else
+            {
+                con.src="./icons/invalidKey.png";
+                setTimeout(() => {
+                    con.src="./icons/blankKey.png";
+                }, 1000);
+            }
+        }
+  
+      
+    });
+}
+let runKeychange=true;
 async function changeDot()
 {
-    blockInput = true;
-    let con = document.getElementById("dotKey"); 
-    con.src="./icons/blankKey.png";
-    con.style.animation = "pulseKey 700ms infinite";
-    await waitKey();
-    while(key == dashKey || key == spaceKey || !validKeys.includes(key))
+    if(runKeychange)
     {
-        con.src="./icons/invalidKey.png";
-        setTimeout(() => {
-            con.src="./icons/blankKey.png";
-        }, 1000);
-        await waitKey();
-    }
-    dotKey = key;
-    con.style.animation = "";
-    if(key == '/')
-        con.src = "./icons/keys/keyslash.png";
-    else if (key == '<')
-        con.src = "./icons/keys/keyleft.png";
-    else if (key == '>')
-        con.src = "./icons/keys/keyright.png";
-    else
-        con.src = `./icons/keys/key${key}.png`;
+        runKeychange = false;
+        blockInput = true;
         
+        let con = document.getElementById("dotKey"); 
+        con.src="./icons/blankKey.png";
+        con.style.animation = "pulseKey 700ms infinite";
 
-    blockInput = false;
+        await waitKey(con);
+        dotKey = key;
+        con.style.animation = "";
+
+        if(key == '/')
+            con.src = "./icons/keys/keyslash.png";
+        else if (key == ',')
+            con.src = "./icons/keys/keyleft.png";
+        else if (key == '.')
+            con.src = "./icons/keys/keyright.png";
+        else
+            con.src = `./icons/keys/key${key}.png`;
+
+        blockInput = false;
+        runKeychange = true;
+    }
+}
+async function changeDash()
+{
+    if(runKeychange)
+    {
+        runKeychange = false;
+        blockInput = true;
+
+        let con = document.getElementById("dashKey"); 
+        con.src="./icons/blankKey.png";
+        con.style.animation = "pulseKey 700ms infinite";
+
+        await waitKey(con);
+        dashKey = key;
+        con.style.animation = "";
+
+        if(key == '/')
+            con.src = "./icons/keys/keyslash.png";
+        else if (key == ',')
+            con.src = "./icons/keys/keyleft.png";
+        else if (key == '.')
+            con.src = "./icons/keys/keyright.png";
+        else
+            con.src = `./icons/keys/key${key}.png`;
+            
+        blockInput = false;
+        runKeychange = true;
+    }
+}
+async function changeSpace()
+{
+    if(runKeychange)
+    {
+        runKeychange = false;
+        blockInput = true;
+
+        let con = document.getElementById("spaceKey"); 
+        con.src="./icons/blankKey.png";
+        con.style.animation = "pulseKey 700ms infinite";
+
+        await waitKey(con);
+        spaceKey = key;
+        con.style.animation = "";
+
+        if(key == '/')
+            con.src = "./icons/keys/keyslash.png";
+        else if (key == ',')
+            con.src = "./icons/keys/keyleft.png";
+        else if (key == '.')
+            con.src = "./icons/keys/keyright.png";
+        else
+            con.src = `./icons/keys/key${key}.png`;
+            
+        blockInput = false;
+        runKeychange = true;
+    }
 }
 
 let dotKey = 'j', dashKey = 'k', spaceKey = 'h';
@@ -471,7 +546,7 @@ document.onkeydown = function(e){
             dash();
         else if (key ==spaceKey)//h
             space();
-        else if(key===67) // c
+        else if(key== "[") // c
             swapWords();
     }
     else
